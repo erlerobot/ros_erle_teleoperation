4dda20be17af99a50c842b8868c6892a5233c293
Martin Beentjes <contact@martinbeentjes.nl>
2015-11-05 14:54:21 +0100

    As of mavros 0.15 (0.14 in reality) the namespace of the messages is moved from `mavros` to `mavros_msgs`. This change is implemented in this commit. Also refactored some small bits and pieces.

--- src/ROS/mavros_setstreamrate.cpp
+++ src/ROS/mavros_setstreamrate.cpp
@@ -3,39 +3,40 @@
 MAVROS_setStreamRate::MAVROS_setStreamRate()
 {
     ros::NodeHandle n;
-    ros::ServiceClient client = n.serviceClient<mavros::StreamRate>("/mavros/set_stream_rate");
-    mavros::StreamRate srv;
+    ros::ServiceClient client = n.serviceClient<mavros_msgs::StreamRate>("/mavros/set_stream_rate");
+    mavros_msgs::StreamRate srv;
     srv.request.stream_id = 0;
     srv.request.message_rate = 100;
     srv.request.on_off = 1;
-    if(client.call(srv)){
-        ROS_INFO("send ok");
-    }else{
+
+    if (client.call(srv)) {
+        ROS_INFO("Send OK");
+    } else {
         ROS_INFO("Failed to call service");
     }
 
-    ros::ServiceClient cl_get = n.serviceClient<mavros::ParamGet>("/mavros/param/get");
-    mavros::ParamGet paramget;
+    ros::ServiceClient cl_get = n.serviceClient<mavros_msgs::ParamGet>("/mavros/param/get");
+    mavros_msgs::ParamGet paramget;
     paramget.request.param_id = "SYSID_MYGCS";
 
-    ros::ServiceClient cl = n.serviceClient<mavros::ParamSet>("/mavros/param/set");
-    while(true){
-        if(cl_get.call(paramget)){
-            ROS_INFO("send ok %d value: %d", paramget.response.success, paramget.response.integer);
-            if(paramget.response.integer==1){
+    ros::ServiceClient cl = n.serviceClient<mavros_msgs::ParamSet>("/mavros/param/set");
+    for(;;) {
+        if (cl_get.call(paramget)) {
+            ROS_INFO("Send OK %d Value: %d", paramget.response.success, paramget.response.value.integer);
+            if (paramget.response.value.integer == 1) {
                 break;
-            }else{
-                mavros::ParamSet paramset2;
+            } else {
+                mavros_msgs::ParamSet paramset2;
                 paramset2.request.param_id = "SYSID_MYGCS";
-                paramset2.request.integer = 1;
-                paramset2.request.real = 1;
-                if(cl.call(paramset2)){
+                paramset2.request.value.integer = 1;
+                paramset2.request.value.real = 1;
+                if (cl.call(paramset2)) {
                     ROS_INFO("SYSIS_MYGCS: send ok");
-                }else{
+                } else {
                     ROS_INFO("Failed to call service SYSIS_MYGCS");
                 }
             }
-        }else{
+        } else {
             ROS_ERROR("Failed GET PARAMETER");
         }
     }
